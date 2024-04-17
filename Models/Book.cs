3c290f31c68914dd08bcc132b17f703f3277e7d2
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-04-17 13:22:47 +0200

    Created required models

---
+++ Models/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateExample.Models {
+    internal class Book {
+        public int ID { get; set; }
+        public string ISBN { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ Models/Borrow.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateExample.Models {
+    internal class Borrow {
+        public int ID { get; set; }
+        public string ClientID { get; set; }
+        public string BookID { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+
+        public bool IsReturned() {
+            return ReturnDate != DateTime.MinValue;
+        }
+    }
+}
