0065b98d16dc001e4a39edd6c019ba1df41398fa
Sebastian Gałan <sebastian.galan7625@gmail.com>
2024-04-17 14:58:20 +0200

    Added a database session manager

---
+++ Managers/DatabaseSessionManager.cs
@@ -0,0 +1,28 @@
+﻿using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHibernateExample.Managers {
+    internal class DatabaseSessionManager {
+        private static ISessionFactory mSessionFactory;
+
+        public static ISessionFactory SessionFactory {
+            get {
+                if(mSessionFactory == null) {
+                    var configuration = new Configuration();
+                    configuration.Configure();
+                    mSessionFactory = configuration.BuildSessionFactory();
+                }
+                return mSessionFactory;
+            }
+        }
+
+        public static ISession OpenSession() {
+            return SessionFactory.OpenSession();
+        }
+    }
+}
